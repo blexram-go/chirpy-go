a8e3b3ab0c7e5b474ab1be29cff7ce22088c0bc8
brianalexramos <brian_ramos_14@yahoo.com>
2024-06-11 17:46:48 -0700

    Create middleware for handlers

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
-const port = "8080"
-const filePathRoot = "."
+type apiConfig struct {
+	fileserverHits int
+}
 
 func main() {
+	const port = "8080"
+	const filePathRoot = "."
+	cfg := apiConfig{
+		fileserverHits: 0,
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
-	mux.HandleFunc("/healthz/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(http.StatusText(http.StatusOK)))
-	})
+	mux.Handle("/app/*", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))))
+	mux.HandleFunc("/healthz/", handlerReadiness)
+	mux.HandleFunc("/metrics", cfg.handlerMetrics)
+	mux.HandleFunc("/reset", cfg.handlerReset)
+
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -23,4 +30,27 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s", filePathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d\n", cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
